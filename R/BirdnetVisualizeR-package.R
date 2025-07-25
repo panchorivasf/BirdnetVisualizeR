da7f366a1b7e80848cd65e43c58154894e4146c3
panchorivasf <fcorivasf@gmail.com>
2025-07-25 11:08:13 -0400

    update main help file

--- R/BirdnetVisualizeR-package.R
+++ R/BirdnetVisualizeR-package.R
@@ -1,6 +1,9 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
+#' BirdnetVisualizeR: Fast plots for BirNET outputs
+#'
+#' This package provides several tools to visualize the CSV outputs from
+#' BirdNET.
+#'
+#' @docType package
+#' @name BirdnetVisualizeR-package
+#' @aliases BirdnetVisualizeR
 NULL
